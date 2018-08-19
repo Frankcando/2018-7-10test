2f083a3cdea8e2bc5e08f567866072a5f3241e56
frank <diablof@gmail.com>
2018-08-19 15:06:34 +0800

    bicycle project

---
+++ bicycles.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+bicycles = [ 'trek', 'cannon',   'redline', 'AAAAAA'  ]
+
+
+
+print(bicycles[0].title())
+print(bicycles[1].upper())
+print(bicycles[-1].lower())
+
+print(bicycles[-2].upper())
+
+message = " my first bicyle was a " +bicycles[1].title() + "."
+print(message)
+
